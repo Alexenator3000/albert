f08bbb087bcbb04a728c430227955f32cf82b4aa
Alexenator3000 <alekseyyaschenko2009@gmail.com>
2023-10-15 23:07:10 +0300

    Калькулятор 3.0

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-a = int(input('Введите число 1 '))
+a = input('Введите число 1 ')
+if a.isnumeric() != True:
+    print('nononononono')
+    exit('error')
+a = int(a)
 c = input('Введите операцию ')
-b = int(input('Введите число 2 '))
+if c != '+' and c != '-' and c != '/' and c != '*':
+    print('nononononono')
+    exit('error')
+b = input('Введите число 2 ')
+if b.isnumeric() != True:
+    print('nononononono')
+    exit('error')
+b = int(b)
 if c == '+':
     print(a + b)
 if c == '-':
@@ -9,5 +20,6 @@ if c == '*':
     print(a * b)
 if c == '/' and b == 0:
     print('НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ')
+    exit('error')
 elif c == '/' and b != 0:
     print(a / b)
