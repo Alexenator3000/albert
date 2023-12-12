a7a740902bbc01ae78f638d45da23339684bb88a
Alexenator3000 <alekseyyaschenko2009@gmail.com>
2023-12-12 22:44:55 +0300

    count time

--- main.py
+++ main.py
@@ -1,25 +1,14 @@
-a = input('Введите число 1 ')
-if a.isnumeric() != True:
-    print('nononononono')
-    exit('error')
-a = int(a)
-c = input('Введите операцию ')
-if c != '+' and c != '-' and c != '/' and c != '*':
-    print('nononononono')
-    exit('error')
-b = input('Введите число 2 ')
-if b.isnumeric() != True:
-    print('nononononono')
-    exit('error')
-b = int(b)
-if c == '+':
-    print(a + b)
-if c == '-':
-    print(a - b)
-if c == '*':
-    print(a * b)
-if c == '/' and b == 0:
-    print('НА НОЛЬ ДЕЛИТЬ НЕЛЬЗЯ')
-    exit('error')
-elif c == '/' and b != 0:
-    print(a / b)
+import time
+a = int(input())
+print(a)
+def some_function(a):
+    x = 1
+    c = []
+    while x <= a:
+        c.append(x)
+        x += 1
+    return c
+startt = time.time()
+print(some_function(a))
+endt = time.time()
+print(startt-endt)
